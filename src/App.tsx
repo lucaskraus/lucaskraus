28020f89a46e4c3206c5ca80353519343cbfd71f
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-17 14:20:22 -0300

    Integrate Introduction component into App, replacing inline profile details for a more modular structure and improved readability.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import DarkVeil from '@/components/DarkVeil'
 import './App.css'
 import Header from '@/components/Header'
 import ProfileCard from '@/components/ProfileCard'
+import Introduction from '@/components/Introduction'
 
 function App() {
   return (
@@ -14,15 +15,7 @@ function App() {
         />
         <div className="flex flex-row justify-center gap-16 items-center w-full">
           <ProfileCard />
-          <div className="flex flex-col gap-6">
-            <h2 className="text-2xl">
-              My name is{' '}
-              <span className="font-bold text-blue-500">Lucas Kraus</span> and I
-              am a
-            </h2>
-            <h1 className="text-4xl font-medium">Software Engineer</h1>
-            <img src="https://skillicons.dev/icons?i=html,css,js,ts,react,nextjs,vue,nuxtjs,tailwind,nodejs,$theme=dark" />
-          </div>
+          <Introduction />
         </div>
       </div>
     </div>
