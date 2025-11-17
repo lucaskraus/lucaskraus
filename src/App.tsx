e6e9aa67915ac525710c4b5fb6ca9e95597d9b88
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-17 14:45:42 -0300

    Update Header items in App component and enhance Projects layout with improved styling and description for better user engagement.

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <div className="flex h-screen w-screen flex-col relative overflow-x-hidden overflow-y-auto">
       <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center px-20 py-16 gap-24">
         <Header
-          items={['Skills', 'Career', 'Projects', 'Talk with me']}
+          items={['Skills', 'Career', 'Repositories', 'Talk with me']}
           onPress={() => {}}
         />
         <div className="flex flex-row justify-center gap-16 items-center w-full">
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,10 @@
 export default function Projects() {
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className="flex flex-col gap-10 flex-wrap">
+      <div className="flex flex-col items-center w-full gap-2">
+        <h1 className="text-2xl font-medium">Projects</h1>
+        <p className="text-base">Some of my GitHub repositories</p>
+      </div>
     </div>
   )
 }
