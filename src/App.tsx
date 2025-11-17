6404fb284b4af8dedc79725639fde5e2c63c2d2c
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-17 12:13:49 -0300

    Remove ProfileCard component from App and delete its implementation to streamline the layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import DarkVeil from '@/components/DarkVeil'
 import './App.css'
 import Header from '@/components/Header'
-import ProfileCard from '@/components/ProfileCard'
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <div className='absolute top-0 left-0 w-full h-full flex flex-col items-center p-20 gap-24'>
         <Header items={['Home', 'About', 'Contact']} onPress={() => {}} />
         <div className='flex flex-row justify-center gap-16 items-center w-full'>
-          <ProfileCard />
           <div className='flex flex-col gap-3'>
             <h2 className='text-2xl'>My name is <span className='font-bold text-blue-500'>Lucas Kraus</span> and I am a</h2>
             <h1 className='text-4xl font-bold'>Software Engineer</h1>
