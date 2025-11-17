6ea6307e1ba4778ddf20e395663465562e1133cd
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-17 10:59:52 -0300

    Update layout of App component to center ProfileCard and improve spacing between elements

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <DarkVeil speed={1.5} />
       <div className='absolute top-0 left-0 w-full h-full flex flex-col items-center p-20 gap-24'>
         <Header items={['Home', 'About', 'Contact']} onPress={() => {}} />
-        <div className='flex flex-row justify-between items-center w-full'>
+        <div className='flex flex-row justify-center gap-24 items-center w-full'>
           <ProfileCard />
           <div className='flex flex-col gap-3'>
             <h2 className='text-2xl'>My name is <span className='font-bold text-blue-500'>Lucas Kraus</span> and I am a</h2>
