5b3d7f15ec1ce58b671c153bd12c89216c867f19
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-17 09:47:40 -0300

    Refine README.md for clarity and updated instructions

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import DarkVeil from '@/components/DarkVeil'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex flex-1 h-screen w-screen flex-col relative">
+      <DarkVeil speed={1.5} />
+    </div>
+  )
+}
+
+export default App
