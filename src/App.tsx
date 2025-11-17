fc512ac4131db67b7853a1755a6f4a58ffbce48e
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-17 10:43:27 -0300

    Add profile image to introduction section in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ function App() {
   return (
     <div className="flex h-screen w-screen flex-col relative">
       <DarkVeil speed={1.5} />
-      <div className='absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center'>
-        <div className='flex flex-col gap-2'>
+      <div className='absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center p-24'>
+        <div className='flex flex-col gap-3'>
           <h2 className='text-2xl'>My name is <span className='font-bold text-blue-500'>Lucas Kraus</span> and I am a</h2>
           <h1 className='text-4xl font-bold'>Software Engineer</h1>
+          <img src="https://skillicons.dev/icons?i=html,css,js,ts,react,nextjs,vue,tailwind,nodejs,$theme=dark" />
         </div>
       </div>
     </div>
