b440bbd14982c5b9c53629ab117f679392fb30c4
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-17 10:37:03 -0300

    Add introduction section with name and title in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="flex flex-1 h-screen w-screen flex-col relative">
       <DarkVeil speed={1.5} />
+      <div className='absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center'>
+        <div className='flex flex-col gap-2'>
+          <h2 className='text-2xl'>My name is <span className='font-bold text-blue-500'>Lucas Kraus</span> and I am a</h2>
+          <h1 className='text-4xl font-bold'>Software Engineer</h1>
+        </div>
+      </div>
     </div>
   )
 }
