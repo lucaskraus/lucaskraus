9df9dbd091c9cce319c1dfda44ada4f2d60bd50e
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-17 16:24:29 -0300

    Update package dependencies and enhance AboutMe component with new tab content and layout improvements for better user experience.

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -8,27 +8,51 @@ const TAB_MENU = [
     id: 'introduction',
     title: 'Introduction',
     icon: <AppWindowMacIcon className="text-inherit" />,
+    content:
+      'Mobile & Front-End Engineer with over 8 years of experience in the industry. Specialized in building scalable and efficient web applications using React Native, React, Next.js, Vue.js, Nuxt.js, Tailwind CSS, and Node.js.',
+    image: 'https://skillicons.dev/icons?i=html',
   },
   {
     id: 'certificates',
     title: 'Certificates',
     icon: <TrophyIcon className="text-inherit" />,
+    content: 'Certificates',
+    image: 'https://skillicons.dev/icons?i=html',
   },
   {
     id: 'skills',
     title: 'Skills',
     icon: <BrainIcon className="text-inherit" />,
+    content: 'Skills',
+    image: 'https://skillicons.dev/icons?i=html',
   },
 ]
+
+const TabCard = ({ id }: { id: string }) => {
+  const section = TAB_MENU.find(tab => tab.id === id)
+
+  return (
+    <div className="flex flex-row rounded-xl p-4 gap-4 bg--200">
+      <img
+        src={section?.image}
+        alt={section?.title}
+        className="w-24 h-24 rounded-xl"
+      />
+      <div className="flex flex-col gap-2 items-start">
+        <p className="text-base">{section?.content}</p>
+      </div>
+    </div>
+  )
+}
 
 export default function AboutMe() {
   const [activeTab, setActiveTab] = useState<string>('introduction')
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 w-full">
       <div className="flex flex-col items-center w-full gap-2">
         <h1 className="text-2xl font-medium">About Me</h1>
       </div>
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row gap-8 items-center w-full mx-auto max-w-5xl">
         <div className="flex flex-col gap-2">
           {TAB_MENU.map(tab => (
             <motion.div
@@ -53,10 +77,10 @@ export default function AboutMe() {
             </motion.div>
           ))}
         </div>
-        <div className="flex flex-col gap-2">
-          {activeTab === 'introduction' && <div>Introduction</div>}
-          {activeTab === 'certificates' && <div>Certificates</div>}
-          {activeTab === 'skills' && <div>Skills</div>}
+        <div className="flex flex-col gap-2 flex-1">
+          {activeTab === 'introduction' && <TabCard id="introduction" />}
+          {activeTab === 'certificates' && <TabCard id="certificates" />}
+          {activeTab === 'skills' && <TabCard id="skills" />}
         </div>
       </div>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.glb'],
   plugins: [
     tailwindcss(),
     react({
