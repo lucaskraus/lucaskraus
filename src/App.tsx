b51c0d13e2e60f95a27b1880bcd5c0b861536678
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-17 12:05:18 -0300

    Add Roboto Mono font to index.html and define font variable in CSS; adjust spacing in App component for better layout

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
       <DarkVeil speed={1.5} />
       <div className='absolute top-0 left-0 w-full h-full flex flex-col items-center p-20 gap-24'>
         <Header items={['Home', 'About', 'Contact']} onPress={() => {}} />
-        <div className='flex flex-row justify-center gap-24 items-center w-full'>
+        <div className='flex flex-row justify-center gap-16 items-center w-full'>
           <ProfileCard />
-          <div className='flex flex-col gap-4'>
+          <div className='flex flex-col gap-3'>
             <h2 className='text-2xl'>My name is <span className='font-bold text-blue-500'>Lucas Kraus</span> and I am a</h2>
             <h1 className='text-4xl font-bold'>Software Engineer</h1>
             <img src="https://skillicons.dev/icons?i=html,css,js,ts,react,nextjs,vue,tailwind,nodejs,$theme=dark" />
