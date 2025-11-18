0f7c771903fc194d24c575040f7d8bceca39515e
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-18 13:35:58 -0300

    Refactor Timeline component to enhance item selection handling, improve layout with motion elements, and streamline the display of timeline cards for better user experience.

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,6 +1,5 @@
 import { motion } from 'motion/react'
 import { useState } from 'react'
-import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react'
 
 const TIMELINE_ITEMS = [
   {
@@ -39,72 +38,87 @@ interface TimelineItemProps {
   title: string
   label: string
   description: string
-  index: number
-  selectedIndex: number
+  selected?: boolean
+}
+
+const TimelineCard = ({ title, label, description }: TimelineItemProps) => {
+  return (
+    <motion.div className="flex flex-row gap-2 items-center rounded-xl p-4 bg-gray-400">
+      <div className="flex flex-col gap-2">
+        <img
+          src="/logo.svg"
+          alt="card image"
+          className="w-10 h-10 rounded-full"
+        />
+        <motion.p
+          layoutId={`timeline-card-title-${title}`}
+          className="font-vt323 text-lg text-center"
+        >
+          {title}
+        </motion.p>
+      </div>
+
+      <div className="flex flex-col w-64 items-center justify-center">
+        <motion.h2
+          layoutId={`timeline-card-label-${label}`}
+          className="text-xl font-vt323"
+        >
+          {label}
+        </motion.h2>
+        <motion.p
+          layoutId={`timeline-card-description-${title}`}
+          className="font-vt323 text-lg text-center"
+        >
+          {description}
+        </motion.p>
+      </div>
+    </motion.div>
+  )
 }
 
 const TimelineItem = ({
   title,
   label,
   description,
-  index,
-  selectedIndex,
+  selected,
 }: TimelineItemProps) => {
-  return (
+  return selected ? (
+    <TimelineCard title={title} label={label} description={description} />
+  ) : (
     <div className="flex flex-col gap-2 items-center">
-      <h1 className="text-xl font-medium">{title}</h1>
-      <p className="font-vt323 text-lg text-center">{label}</p>
+      <motion.h1
+        layoutId={`timeline-item-title-${title}`}
+        className="text-xl font-medium"
+      >
+        {title}
+      </motion.h1>
+      <motion.p
+        layoutId={`timeline-item-label-${title}`}
+        className="font-vt323 text-lg text-center"
+      >
+        {label}
+      </motion.p>
       <div className="relative flex items-center justify-center">
         <div className="h-2 w-2 bg-blue-400 rounded-full" />
-        {selectedIndex === index && (
-          <motion.div
-            layoutId="balloon"
-            key="balloon"
-            transition={{ ease: 'easeInOut' }}
-            className="absolute top-4 left-1/2 -translate-x-1/2 z-10 w-max max-w-[90vw]"
-          >
-            <div className="rounded-md bg-gray-900 px-3 py-2 shadow-lg text-center">
-              <motion.span className="text-white text-sm">
-                {description}
-              </motion.span>
-            </div>
-          </motion.div>
-        )}
       </div>
     </div>
   )
 }
 
 export default function Timeline() {
-  const [selectedIndex, setSelectedIndex] = useState<number>(0)
-
-  const handleSelectIndex = (index: number) => {
-    if (index < 0) {
-      setSelectedIndex(0)
-    } else if (index >= TIMELINE_ITEMS.length) {
-      setSelectedIndex(TIMELINE_ITEMS.length - 1)
-    } else {
-      setSelectedIndex(index)
-    }
-  }
+  const [selectedItem, setSelectedItem] = useState<TimelineItemProps | null>(
+    TIMELINE_ITEMS[0]
+  )
 
   return (
     <div className="flex flex-col gap-10">
-      <div className="flex flex-row justify-between items-center">
-        <ChevronLeftIcon
-          className="cursor-pointer"
-          onClick={() => handleSelectIndex(selectedIndex - 1)}
-        />
+      <div className="flex flex-row items-center justify-center">
         <div className="flex flex-col items-center w-full gap-2">
           <h1 className="text-2xl font-medium select-none">Career</h1>
           <p className="text-lg select-none">
             A brief overview of my career until now
           </p>
         </div>
-        <ChevronRightIcon
-          className="cursor-pointer"
-          onClick={() => handleSelectIndex(selectedIndex + 1)}
-        />
       </div>
       <div className="flex flex-row w-full gap-12 items-center justify-center flex-wrap">
         {TIMELINE_ITEMS.map((item, index) => (
@@ -114,23 +128,16 @@ export default function Timeline() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 + index * 0.1, ease: 'easeInOut' }}
             className="max-w-48 cursor-pointer"
-            onClick={() => setSelectedIndex(index)}
+            onClick={() => setSelectedItem(item)}
             role="button"
             tabIndex={0}
-            onKeyDown={e => {
-              if (e.key === 'Enter' || e.key === ' ') {
-                e.preventDefault()
-                setSelectedIndex(index)
-              }
-            }}
           >
             <TimelineItem
               key={item.title}
               title={item.title}
               label={item.label}
               description={item.description}
-              index={index}
-              selectedIndex={selectedIndex}
+              selected={selectedItem?.title === item.title}
             />
           </motion.div>
         ))}
