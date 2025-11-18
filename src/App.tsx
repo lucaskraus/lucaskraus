b3f25d880b5ccd9e2a8a8e519fb5f10f71c387bc
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-18 10:00:06 -0300

    Add Squares component to App for dynamic visual effects with customizable properties

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,19 @@ import Timeline from '@/components/Timeline'
 import Repositories from '@/components/Repositories'
 import AboutMe from '@/components/AboutMe'
 import Picture from '@/components/Picture'
+import Squares from '@/components/Squares'
 import './App.css'
 
 function App() {
   return (
     <div className="flex h-screen w-screen flex-col relative overflow-x-hidden overflow-y-auto">
+      <Squares
+        speed={0.3}
+        squareSize={40}
+        direction="diagonal"
+        borderColor="#271e37"
+        hoverFillColor="#222222"
+      />
       <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center p-16 gap-24">
         <Header
           items={['About Me', 'Career', 'Repositories', 'Talk with me']}
