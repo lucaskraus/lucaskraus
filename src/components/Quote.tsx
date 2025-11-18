e7670b9ba90aa5fdcede6a6101d80c87140916f4
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-18 20:14:26 -0300

    Update Quote component styling by increasing the header size to 2xl and adjusting the author text size to lg for improved visual hierarchy and readability.

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,10 +1,12 @@
 export default function Quote() {
   return (
     <div className="flex flex-col gap-2 items-center">
-      <h1 className="font-playfair-display text-xl">
+      <h1 className="font-playfair-display text-2xl">
         Make it work, make it right, make it fast.
       </h1>
-      <p className="font-bold text-center font-playfair-display">- Kent Beck</p>
+      <p className="font-bold text-center font-playfair-display text-lg">
+        - Kent Beck
+      </p>
     </div>
   )
 }
