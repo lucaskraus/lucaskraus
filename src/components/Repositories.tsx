b865fdb5ba8f6146c48b240e91a6e9c6eb529031
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-18 10:28:27 -0300

    Update text size in Repositories component for improved readability

--- src/components/Repositories.tsx
+++ src/components/Repositories.tsx
@@ -3,7 +3,7 @@ export default function Projects() {
     <div className="flex flex-col gap-10 flex-wrap">
       <div className="flex flex-col items-center w-full gap-2">
         <h1 className="text-2xl font-medium">Projects</h1>
-        <p className="text-base">Some of my GitHub repositories</p>
+        <p className="text-lg">Some of my GitHub repositories</p>
       </div>
     </div>
   )
