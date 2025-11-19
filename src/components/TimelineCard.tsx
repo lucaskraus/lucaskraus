d22776ad95d25bc0f078d08dff249efcd877f841
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-18 22:06:04 -0300

    Enhance TimelineCard styling by increasing height and adjusting text sizes for improved readability and visual hierarchy.

--- src/components/TimelineCard.tsx
+++ src/components/TimelineCard.tsx
@@ -23,7 +23,7 @@ export default function TimelineCard({
     <motion.div
       layout
       layoutId={`timeline-item-${title}`}
-      className="flex gap-2.5 px-4 py-2 w-80 h-28 shadow-md border border-gray-400 relative will-change-transform"
+      className="flex gap-2.5 px-4 py-2 w-80 h-36 shadow-md border border-gray-400 relative will-change-transform"
       style={{
         borderRadius: 16,
         backgroundImage: `url(${image})`,
@@ -34,8 +34,8 @@ export default function TimelineCard({
     >
       <div className="absolute inset-0 bg-black/50 rounded-2xl" />
       <div className="flex flex-col flex-1 h-full gap-2 w-full z-10">
-        <h2 className="text-lg text-left font-medium font-vt323">{title}</h2>
-        <p className="text-xs text-left">{description}</p>
+        <h2 className="text-xl text-left font-medium font-vt323">{title}</h2>
+        <p className="text-sm text-left">{description}</p>
       </div>
     </motion.div>
   )
