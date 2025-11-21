82b212e455ee1db6d228417febdb02f6e5553c12
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-21 18:45:39 -0300

    Update Header component styles to improve spacing and text size for better responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,12 +14,12 @@ export default function Header({ items }: HeaderProps) {
 
   return (
     <div className="sticky top-8 z-50 flex flex-row w-full lg:px-6 px-2.5 py-4 items-center justify-center rounded-full backdrop-blur-xs border border-white/10 bg-black/10">
-      <div className="flex flex-row gap-8">
+      <div className="flex flex-row gap-4 lg:gap-10">
         {items.map(item => (
           <button
             key={item.label}
             onClick={() => scrollToSection(item.id)}
-            className="text-primary hover:text-white active:scale-95 transition-all duration-300 cursor-pointer tracking-tight text-xs lg:text-base"
+            className="text-primary hover:text-white active:scale-95 transition-all duration-300 cursor-pointer tracking-tight text-[0.65rem] lg:text-base"
           >
             {item.label}
           </button>
